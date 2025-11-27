f1d2d14a3478dbf5126bc31ded67b179a594cb7a
Petr Akimov <petr.akimov@yahoo.com>
2025-11-27 10:00:01 +0300

    bug fix #0

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,13 +1,11 @@
 output "static_access_key_id" {
-  value       = yandex_iam_service_account_static_access_key.static_key.id
-  description = "Access key id (используется для S3 backend и provider)"
+  value = yandex_iam_service_account_static_access_key.static_key.access_key
 }
 
 output "static_access_key_secret" {
-  value       = yandex_iam_service_account_static_access_key.static_key.secret
-  description = "Секрет ключа (чувствительный)"
-  sensitive   = true
+  value = yandex_iam_service_account_static_access_key.static_key.secret_key
 }
+
 
 output "bucket" {
   value       = yandex_storage_bucket.tfstate.bucket
