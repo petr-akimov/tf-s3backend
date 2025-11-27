3a059afbded84fde2ff3d34448cca5c528de3298
Petr Akimov <petr.akimov@yahoo.com>
2025-11-27 10:20:21 +0300

    bug fix #5

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -22,13 +22,6 @@ resource "yandex_iam_service_account" "tf_sa" {
   description = "Service account for Terraform (tfstate & infra)"
 }
 
-# Bind storage.admin to the service account at cloud level (member - safer)
-resource "yandex_resourcemanager_cloud_iam_member" "sa_storage" {
-  cloud_id = var.cloud_id
-  role     = "roles/storage.admin"
-  member   = "serviceAccount:${yandex_iam_service_account.tf_sa.id}"
-}
-
 
 # Create static access key for Object Storage
 resource "yandex_iam_service_account_static_access_key" "static_key" {
@@ -47,3 +40,13 @@ resource "yandex_storage_bucket" "tfstate" {
   folder_id = var.folder_id
 }
 
+
+resource "yandex_storage_bucket_grant" "tfstate_sa_grant" {
+  bucket = yandex_storage_bucket.tfstate.bucket
+
+  grant {
+    type        = "CanonicalUser"
+    id          = yandex_iam_service_account.tf_sa.id
+    permissions = ["FULL_CONTROL"]
+  }
+}
